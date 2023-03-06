dc09db1354bd42d94f419dd5230d9a5aed62bfc2
zty <15010972518@163.com>
2023-03-06 12:53:50 +0800

    test: add minus tests

--- src/features/calculator/calculatorSlice.spec.ts
+++ src/features/calculator/calculatorSlice.spec.ts
@@ -157,6 +157,71 @@ describe('calculator reducer', () => {
 
                 expect(actual.displayValue).toEqual('2')
             });
+
+            it('should result in negative integer number', () => {
+                customInitialState('3')
+
+                let actual = calculatorSlice.reducer(initialState, minus())
+                actual = calculatorSlice.reducer(actual, inputNumber('4'))
+                actual = calculatorSlice.reducer(actual, calculate())
+
+                expect(actual.displayValue).toEqual('-1')
+            });
+        })
+
+        describe('minus on float number', () => {
+            it('should perform basic minus on float number', () => {
+                customInitialState('3.1')
+
+                let actual = calculatorSlice.reducer(initialState, minus())
+                actual = calculatorSlice.reducer(actual, inputNumber('1.2'))
+                actual = calculatorSlice.reducer(actual, calculate())
+
+                expect(Number(actual.displayValue)).toBeCloseTo(1.9)
+            });
+
+            it('should result in negative float number', () => {
+                customInitialState('3.1')
+
+                let actual = calculatorSlice.reducer(initialState, minus())
+                actual = calculatorSlice.reducer(actual, inputNumber('4.2'))
+                actual = calculatorSlice.reducer(actual, calculate())
+
+                expect(Number(actual.displayValue)).toBeCloseTo(-1.1)
+            });
+        })
+
+        describe('continuous minus', () => {
+            it('should perform continuous minus when click `-`', () => {
+                customInitialState('5')
+
+                let actual = calculatorSlice.reducer(initialState, minus())
+                actual = calculatorSlice.reducer(actual, inputNumber('2'))
+                actual = calculatorSlice.reducer(actual, minus())
+
+                expect(actual.displayValue).toEqual('3')
+            });
+
+            it('should get correct result after continuous minus by click `-`', () => {
+                customInitialState('5')
+
+                let actual = calculatorSlice.reducer(initialState, minus())
+                actual = calculatorSlice.reducer(actual, inputNumber('2'))
+                actual = calculatorSlice.reducer(actual, minus())
+                actual = calculatorSlice.reducer(actual, inputNumber('2'))
+                actual = calculatorSlice.reducer(actual, calculate())
+
+                expect(actual.displayValue).toEqual('1')
+            });
+
+            it('should result in 0 when consecutively click `-` and `=`', () => {
+                customInitialState('1234')
+
+                let actual = calculatorSlice.reducer(initialState, minus())
+                actual = calculatorSlice.reducer(actual, calculate())
+
+                expect(actual.displayValue).toEqual('0')
+            });
         })
     })
 })
